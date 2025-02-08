e128f884f138d95024ee44d432d5a02b1df4ca87
Allex Welter <allexwelter13@gmail.com>
2025-02-08 08:09:39 -0300

    .

--- src/pages/Questionario.jsx
+++ src/pages/Questionario.jsx
@@ -1,14 +1,31 @@
 import { use, useState } from "react"
 import { Button, Card, Container, Form } from "react-bootstrap"
 
+
+const questao = [{
+    questao: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2", "Alternartiva 3", "Alternativa 4"]
+}]
+
+
 function Questionario() {
 
     const [perguntaAtual, setPerguntaAtual] = useState(0)
     const [respostaSelecionada, setRespostaSelecionada] = useState()
 
+    const irProxima = () => {
+        if (perguntaAtual < questao.length - 1) {
+            setPerguntaAtual(perguntaAtual + 1)
+            setRespostaSelecionada(null)
+        }
+    }
 
-
-
+    const irAnterior = () => {
+        if (perguntaAtual > 0) {
+            setPerguntaAtual(perguntaAtual - 1)
+            setPerguntaAtual(null)
+        }
+    }
 
     
     return(
@@ -23,7 +40,7 @@ function Questionario() {
                     </Form>
                     <div className="mt-3">
                         <Button variant="secondary">
-                          Anterior  
+                            Anterior  
                         </Button>
                         <Button variant="primary">
                             Próxima
