a46c37b5ea196e1c722ecaf2ae80079abcef9eb9
Allex Welter <allexwelter13@gmail.com>
2025-02-10 07:10:57 -0300

    .

--- src/pages/Resultado.jsx
+++ src/pages/Resultado.jsx
@@ -1,12 +1,22 @@
+import { Button, Card, CardTitle, Container } from "react-bootstrap"
 
 
 function Resultado({pontuacao}) {
 
     return(
-        <>
-        
-        
-        </>
+        <Container className="d-flex justify-content-center align-items-center" style={{minHeight: "100vh"}}>
+            <Card className="text-center" style={{width: "18rem"}}>
+                <Card.Body>
+                    <CardTitle>Parabéns</CardTitle>
+                    <Card.Text>
+                        Oi Fulano, seu resultado foi {pontuacao}/10 !
+                    </Card.Text>
+                    <Button variant="warning" onClick={() => {{/*//logica para enviar por email*/}}}> 
+                        ENVIAR POR EMAIL    
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
 
--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Questionario } from "../pages/Questionario";
+import { Resultado } from "../pages/Resultado";
 
 function RouteApp() {
     return(
@@ -8,6 +9,7 @@ function RouteApp() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/questionario" element={<Questionario />} />
+                <Route path="/resultado" element={<Resultado />} />
             </Routes>
         </BrowserRouter>
     )
